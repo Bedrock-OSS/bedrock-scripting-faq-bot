898d761fed23e30b813c9131c51d5fc53bfdb5a1
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2022-12-09 09:26:16 +0700

    Drop footer for faqs missing modification time

--- cogs/faq.py
+++ cogs/faq.py
@@ -51,11 +51,12 @@ class Faq(commands.Cog):
                 color=discord.Color.blurple(),
             )
             embed.set_thumbnail(url=img if (img := faq.image) else '')
-            embed.timestamp = datetime.datetime.fromtimestamp(faq.modification_time)
+            if(faq.modification_time != 0):
+                embed.timestamp = datetime.datetime.fromtimestamp(faq.modification_time)
+                embed.set_footer(text="Last updated:")
 
         # embed.set_footer(text=Texts.EMBED_FOOTER.format(
         #     self.bot.user.name))  # type: ignore
-        embed.set_footer(text="Last updated:")
 
         return embed
 
